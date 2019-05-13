53b876f64eda8e56f6f1a2349f3f51ea073e40e7
Vadim <vadik.napalkov@yandex.ru>
2019-05-13 17:26:34 +0300

    database dialect in config

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -2,7 +2,7 @@ import config from "./config";
 
 const appConfig = {
   env: process.env.NODE_ENV || config.env,
-  port: parseInt(process.env.PORT, 10) || config.port,
+  port: parseInt(process.env.PORT, 10) || Number(config.port),
   domain: process.env.DOMAIN || config.domain,
   corsDomain: process.env.CORS_DOMAIN || config.corsDomain,
   frontDomain: process.env.FRONT_DOMAIN || config.frontDomain,
@@ -13,7 +13,7 @@ const appConfig = {
     password: process.env.DATABASE_PASSWORD || config.configDatabase.password,
     database: process.env.DATABASE_NAME || config.configDatabase.database,
     host: process.env.DATABASE_DEVELOPMENT_HOST || config.configDatabase.host,
-    dialect: "postgres"
+    dialect: process.env.DATABASE_DIALECT || config.configDatabase.dialect
   }
 };
 
